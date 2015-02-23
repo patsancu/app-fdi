8507539d8ec9a4ea0e9f2f9614101797bc8a0c05
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2015-02-23 17:51:22 +0100

    Haciendo limpieza

--- src/main/java/es/ucm/fdi/storage/business/control/StorageObjectRepository.java
+++ src/main/java/es/ucm/fdi/storage/business/control/StorageObjectRepository.java
@@ -5,24 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import es.ucm.fdi.storage.business.entity.StorageObject;
 import es.ucm.fdi.storage.business.entity.StorageObjectId;
 
-public interface StorageObjectRepository extends CrudRepository<StorageObject, StorageObjectId>{
-//	
-//
-//	@Autowired
-//	private SessionFactory sf;
-//
-//	public void save(StorageObject object) {
-//		Session session = sf.getCurrentSession();		
-//		session.saveOrUpdate(object);
-//	}
-//
-//	public StorageObject getById(StorageObjectId storageObjectId) {
-//		Session session = sf.getCurrentSession();
-//		return (StorageObject) session.get(StorageObject.class, storageObjectId);
-//	}
-//
-//	public void delete(StorageObject object) {
-//		Session session = sf.getCurrentSession();
-//		session.delete(object);
-//	}
+public interface StorageObjectRepository extends CrudRepository<StorageObject, StorageObjectId> {
+
 }
