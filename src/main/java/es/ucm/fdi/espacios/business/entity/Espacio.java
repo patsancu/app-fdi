8205e0ea433edd4068f4eeb314c952b0b07f7dab
Patrick <patrick@sanzo.es>
2015-03-02 21:18:47 +0100

    Añadido "cascade" en clase Espacio

--- src/main/java/es/ucm/fdi/espacios/business/entity/Espacio.java
+++ src/main/java/es/ucm/fdi/espacios/business/entity/Espacio.java
@@ -2,6 +2,7 @@ package es.ucm.fdi.espacios.business.entity;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -25,7 +26,7 @@ public class Espacio {
 	@Column(name="ESPACIO_ID")
 	private Long id;
 	
-	@OneToMany(mappedBy="espacio")
+	@OneToMany(mappedBy="espacio", cascade={CascadeType.ALL})
 	private List<Reserva> reservas;
 	
 	private String nombre;
