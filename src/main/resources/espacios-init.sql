8daf711a2e2643ea23ef69a88b46a61304057ecc
Patrick <patrick@sanzo.es>
2015-03-10 12:40:23 +0100

    Modificación script

--- src/main/resources/espacios-init.sql
+++ src/main/resources/espacios-init.sql
@@ -3,6 +3,6 @@ insert into Espacio (ESPACIO_ID, aforo, nombre, notas, tipoEspacio) values (1, 8
 insert into Espacio (ESPACIO_ID, aforo, nombre, notas, tipoEspacio) values (2, 88, 'Auditorio principal', 'Sala cargado por defecto', 2)
 
 
-insert into Reserva (id, aclaraciones, ESPACIO_ID, fechaCreacion, fechaFin, fechaInicio, titular) values (0, 'Es provisional', 1,'2015-02-01 12:30:57', '2015-02-07 12:30:57', '2015-02-07 13:30:57', 'Gerencia')
-insert into Reserva (id, aclaraciones, ESPACIO_ID, fechaCreacion, fechaFin, fechaInicio, titular) values (1, 'Conferencia Stallman', 1,'2015-02-10 10:30:00', '2015-02-10 15:30:00', '2015-02-10 17:30:00', 'Gerencia')
-insert into Reserva (id, aclaraciones, ESPACIO_ID, fechaCreacion, fechaFin, fechaInicio, titular) values (2, 'Ciclo de conferencias', 1,'2015-02-12 10:30:00', '2015-02-22 15:00:00', '2015-02-22 17:30:00', 'Gerencia')
+insert into Reserva (id, aclaraciones, ESPACIO_ID, fechaCreacion, fechaInicio, fechaFin, titular) values (0, 'Es provisional', 1,'2015-03-01 12:30:57', '2015-03-07 12:30:57', '2015-03-07 13:30:57', 'Gerencia')
+insert into Reserva (id, aclaraciones, ESPACIO_ID, fechaCreacion, fechaInicio, fechaFin, titular) values (1, 'Conferencia Stallman', 2,'2015-03-10 10:30:00', '2015-03-10 15:30:00', '2015-03-10 17:30:00', 'Gerencia')
+insert into Reserva (id, aclaraciones, ESPACIO_ID, fechaCreacion, fechaInicio, fechaFin, titular) values (2, 'Ciclo de conferencias', 1,'2015-03-12 10:30:00', '2015-03-22 15:00:00', '2015-03-22 17:30:00', 'Gerencia')
