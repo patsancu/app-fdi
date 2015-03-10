6f901823e35af87b7dfedfa5bbca569c41784776
Patrick <patrick@sanzo.es>
2015-03-10 12:47:26 +0100

    Fetch "voraz"

--- src/main/java/es/ucm/fdi/espacios/business/entity/Espacio.java
+++ src/main/java/es/ucm/fdi/espacios/business/entity/Espacio.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,7 +27,7 @@ public class Espacio {
 	@Column(name="ESPACIO_ID")
 	private Long id;
 	
-	@OneToMany(mappedBy="espacio", cascade={CascadeType.ALL})
+	@OneToMany(mappedBy="espacio", cascade={CascadeType.ALL}, fetch=FetchType.EAGER)
 	private List<Reserva> reservas;
 	
 	private String nombre;
