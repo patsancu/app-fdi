91d383153f507eda52991000eb97194a5e20b632
Patrick <patrick@sanzo.es>
2015-04-17 19:29:43 +0200

    Externalización constantes.

---
+++ src/main/java/es/ucm/fdi/util/Constants.java
@@ -0,0 +1,28 @@
+package es.ucm.fdi.util;
+
+public class Constants {
+	
+	// Reservas
+	public static final String URL_REST_NUEVA_RESERVA = "/reservas/rest/nueva";	
+	public static final String URL_NUEVA_RESERVA = "/reservas/nuevo";
+	public static final String URL_LISTAR_RESERVAS = "/reservas";
+	public static final String URL_RESERVA_INDIVIDUAL = "/reservas/{id}";
+	public static final String URL_CALENDARIO_RESERVAS = "/reservas/calendario";
+	
+	// Redirecciones
+	public static final String URL_REDIRECCIONES = "/u";
+	public static final String URL_NUEVA_REDIRECCION = "/urls/nueva";
+	public static final String URL_REST_NUEVA_REDIRECCION = "/urls/rest/nueva";
+	
+	// Avisos
+	public static final String URL_NUEVO_AVISO = "/avisos/nuevo";
+	public static final String URL_AVISO_INDIVIDUAL = "/avisos/{id}";
+	public static final String URL_LISTAR_AVISOS = "/avisos";
+	
+	// Espacios
+	public static final String URL_NUEVO_ESPACIO = "/espacios/nuevo";
+	public static final String URL_LISTAR_ESPACIOS = "/espacios";
+	public static final String URL_ESPACIO_INDIVIDUAL = "/espacios/{id}";
+}
+
+
